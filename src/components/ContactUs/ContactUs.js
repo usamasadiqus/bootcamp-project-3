4d8ed5b69dd027cc072401bdcab2c7e124f8ecdf
Usama Sadiq <usamasadiq029@gmail.com>
2021-01-25 14:37:34 +0500

    Last component contact.js complete

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,17 +1,23 @@
+import Image from "./../../images/image.jpeg";
 import styles from "./ContactUs.module.css";
 
 const ContactUs = () => {
   return (
     <div className={styles.container}>
-      {/* <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924234.6302710465!2d66.59495074892502!3d25.19338946981612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e06651d4bbf%3A0x9cf92f44555a0c23!2sKarachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1610641157177!5m2!1sen!2s"
-        width="100%"
-        frameborder="0"
-        style={{ border: 0 }}
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0"
-      ></iframe> */}
+      <h1>Get in touch</h1>
+      <div className={styles.form}>
+        <form>
+          <input type="text" placeholder="Full Name" required />
+          <select>
+            <option value="0" selected>Select</option>
+            <option value="feedback">Feedback</option>
+            <option value="suggestion">Suggestion</option>
+          </select>
+          <input type="email" placeholder="Email Address" required />
+          <textarea cols="30" rows="5" placeholder="Message"></textarea>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
     </div>
   );
 };
